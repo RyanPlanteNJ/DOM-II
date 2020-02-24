a6247f947af70bd8d0d7300234bc95a95ccf0597
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-02-23 22:51:58 -0500

    added more events and made the header stay at the top regardless of event

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 // Your code goes here
 
+const nav = document.querySelector('.main-navigation');
+nav.style = "z-index: 1";
+
+
 const header = document.querySelector('.logo-heading');
   header.addEventListener('click', (event) => {
     header.innerText = "Hover Over Some Objects";
-  header.addEventListener('keypress', (event) =>{
-    header.innerText = `You pressed ${input}`
+    header.style.color = "blue";
+  header.addEventListener('dblclick', (event) =>{
+    header.innerText = `Fun Bus`;
   })
 });
 
@@ -12,8 +17,10 @@ const header = document.querySelector('.logo-heading');
 const uppercontent = document.querySelector('.intro');
 uppercontent.addEventListener('click', (event) => {
   uppercontent.style.backgroundColor = 'grey';
+  event.stopPropagation();
   uppercontent.addEventListener('dblclick', (event) => {
     uppercontent.style.backgroundColor = 'transparent';
+    event.stopPropagation();
   });
 });
 
@@ -28,18 +35,26 @@ uppercontentname.addEventListener('click', (event) => {
 });
 
 const content = document.querySelector('.content-section');
-content.addEventListener('mouseover', function() {
-  document.querySelector('.content-section').style = "scale: 1.5; background-color: white; z-index: 1;"; //increases full section image and text included
-  content.addEventListener('mouseleave', function(){
-    document.querySelector('.content-section').style = "scale: 1; z-index: -1";
+content.addEventListener('mouseover', (event) => {
+  content.style = "scale: 1.5; background-color: white; z-index: 1";
+  nav.style = "z-index: 2" //increases full section image and text included
+  event.stopPropagation();
+  content.addEventListener('mouseleave', (event) => {
+    content.style = "scale: 1; z-index: 0";
+    nav.style = "z-index: 1"
+    event.stopPropagation();
   }); //reverts back to normal size
 });
 
 
 const inverse = document.querySelector('.inverse-content');
-inverse.addEventListener('mouseover', function () {
-  document.querySelector('.inverse-content').style = "scale: 1.5; background-color: white; z-index: 1";
-  inverse.addEventListener('mouseleave', function () {
-    document.querySelector('.inverse-content').style = "scale: 1; z-index: -1";
+inverse.addEventListener('mouseover', (event) => {
+  inverse.style = "scale: 1.5; background-color: white; z-index: 1";
+  nav.style = "z-index: 2"
+  event.stopPropagation();
+  inverse.addEventListener('mouseleave', (event) => {
+    inverse.style = "scale: 1; z-index: 0";
+    nav.style = "z-index: 1"
+    event.stopPropagation();
   });
 });
